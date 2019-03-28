0fb3326c116e4e3adc563fd0800bcae5f9a7264d
oruchreis <oruchreis@diyezon.com>
2019-03-28 13:12:55 +0300

    removing test for zookeeper

--- src/Nuve.DataStore.Test/CodisZookeeperStoreProviderTest.cs
+++ src/Nuve.DataStore.Test/CodisZookeeperStoreProviderTest.cs
@@ -10,18 +10,18 @@ namespace Nuve.DataStore.Test
         [TestMethod]
         public void Connectivity()
         {
-            DataStoreManager.DefaultSerializer = new Serializer.JsonNet.JsonNetDataStoreSerializer();
-            DataStoreManager.RegisterProvider("Codis", "Nuve.DataStore.Redis.Zookeeper.CodisZookeeperStoreProvider, Nuve.DataStore.CodisZookeeper");            
-            DataStoreManager.CreateConnection("Codis", "Codis", "51.145.158.172:2181,/codis3_test/codis-cluster/proxy|");            
+            //DataStoreManager.DefaultSerializer = new Serializer.JsonNet.JsonNetDataStoreSerializer();
+            //DataStoreManager.RegisterProvider("Codis", "Nuve.DataStore.Redis.Zookeeper.CodisZookeeperStoreProvider, Nuve.DataStore.CodisZookeeper");            
+            //DataStoreManager.CreateConnection("Codis", "Codis", "51.145.158.172:2181,/codis3_test/codis-cluster/proxy|");            
 
-            var kv = new KeyValueStore();
-            kv.Set("asd", "asd");
+            //var kv = new KeyValueStore();
+            //kv.Set("asd", "asd");
 
 
-            while (true)
-            {
-                Thread.Sleep(1000);
-            }
+            //while (true)
+            //{
+            //    Thread.Sleep(1000);
+            //}
         }
     }
 }
